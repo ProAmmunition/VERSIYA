0beeee14a23c3ef16e2fdabb416767c28212a500
ProAmmunition <37572863+ProAmmunition@users.noreply.github.com>
2018-08-12 13:25:10 +0200

    Login User Interface

---
+++ 25948083_Wassenaar_L_Project_3/25948083_Wassenaar_L_Project_3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_25948083_Wassenaar_L_Project_3.MvcApplication" Language="C#" %>
---
+++ 25948083_Wassenaar_L_Project_3/25948083_Wassenaar_L_Project_3/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace _25948083_Wassenaar_L_Project_3
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
