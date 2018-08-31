201f8af22e8c074037050fa71b63581f7a8e6759
ProAmmunition <37572863+ProAmmunition@users.noreply.github.com>
2018-08-31 19:59:23 +0200

    DB connection

--- 25948083_Wassenaar_L_Project_3/25948083_Wassenaar_L_Project_3/Controllers/RegistrationController.cs
+++ 25948083_Wassenaar_L_Project_3/25948083_Wassenaar_L_Project_3/Controllers/RegistrationController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace _25948083_Wassenaar_L_Project_3.Controllers
 {
     public class RegistrationController : Controller
     {
-        // GET: Registration
-        public ActionResult Registration_page()
+      
+        string connection = "datasource = den1.mysql4.gear.host; port=3306; Initial Catalog = 'versiyadb'; username='versiyadb';password='En5KD_989Z-9';";
+        [HttpGet]
+        public ActionResult Registration_page(Models.UserModel user_model)
         {
-            return View();
+            using (MySqlConnection sql_con = new MySqlConnection(connection))
+            {
+                sql_con.Open();
+                string sql_statement = "INSERT INTO user VALUES(@user_id,@username,@user_password,@user_email_address)";
+                MySqlCommand sql_com = new MySqlCommand(sql_statement,sql_con);
+                sql_com.Parameters.AddWithValue("@user_id", user_model.user_id);
+                sql_com.Parameters.AddWithValue("@username", user_model.username);
+                sql_com.Parameters.AddWithValue("@user_password", user_model.password);
+                sql_com.Parameters.AddWithValue("@user_email_address", user_model.email);
+                sql_com.ExecuteNonQuery();
+
+            }
+                return View();
         }
     }
 }
---
+++ 25948083_Wassenaar_L_Project_3/25948083_Wassenaar_L_Project_3/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _25948083_Wassenaar_L_Project_3.Models
+{
+    public class UserModel
+    {
+        public int user_id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
