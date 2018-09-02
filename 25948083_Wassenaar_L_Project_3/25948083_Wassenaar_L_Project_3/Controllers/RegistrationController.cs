206919000bf0ea0665293cc61a5708fb93743db6
ProAmmunition <37572863+ProAmmunition@users.noreply.github.com>
2018-09-02 17:41:33 +0200

    Database Registration Table Insert

--- 25948083_Wassenaar_L_Project_3/25948083_Wassenaar_L_Project_3/Controllers/RegistrationController.cs
+++ 25948083_Wassenaar_L_Project_3/25948083_Wassenaar_L_Project_3/Controllers/RegistrationController.cs
@@ -10,24 +10,34 @@ namespace _25948083_Wassenaar_L_Project_3.Controllers
 {
     public class RegistrationController : Controller
     {
+       public string connection = "datasource = den1.mysql4.gear.host; port=3306; Initial Catalog = 'versiyadb'; username='versiyadb';password='En5KD_989Z-9';";
+        public ActionResult Registration_page()
+        {
+            return View();
+        }
+        
       
-        string connection = "datasource = den1.mysql4.gear.host; port=3306; Initial Catalog = 'versiyadb'; username='versiyadb';password='En5KD_989Z-9';";
-        [HttpGet]
+        [HttpPost]
         public ActionResult Registration_page(Models.UserModel user_model)
         {
             using (MySqlConnection sql_con = new MySqlConnection(connection))
             {
-                sql_con.Open();
+                
                 string sql_statement = "INSERT INTO user VALUES(@user_id,@username,@user_password,@user_email_address)";
-                MySqlCommand sql_com = new MySqlCommand(sql_statement,sql_con);
-                sql_com.Parameters.AddWithValue("@user_id", user_model.user_id);
-                sql_com.Parameters.AddWithValue("@username", user_model.username);
-                sql_com.Parameters.AddWithValue("@user_password", user_model.password);
-                sql_com.Parameters.AddWithValue("@user_email_address", user_model.email);
-                sql_com.ExecuteNonQuery();
-
+                using(MySqlCommand sql_com = new MySqlCommand(sql_statement))
+                 {
+                    sql_com.Connection = sql_con;
+                    sql_con.Open();
+                    sql_com.Parameters.AddWithValue("@user_id", user_model.user_id);
+                    sql_com.Parameters.AddWithValue("@username", user_model.username);
+                    sql_com.Parameters.AddWithValue("@user_password", user_model.password);
+                    sql_com.Parameters.AddWithValue("@user_email_address", user_model.email);
+                    sql_com.ExecuteNonQuery();
+                    sql_con.Close();
+                }
             }
-                return View();
+                return View(user_model);
+           
         }
     }
 }
