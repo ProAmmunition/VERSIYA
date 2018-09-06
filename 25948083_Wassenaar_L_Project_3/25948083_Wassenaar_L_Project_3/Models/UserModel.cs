ab3fa538a09ad09dca74bbe382650818ab8441a7
ProAmmunition <37572863+ProAmmunition@users.noreply.github.com>
2018-09-06 18:00:42 +0200

    Registration changes

--- 25948083_Wassenaar_L_Project_3/25948083_Wassenaar_L_Project_3/Models/UserModel.cs
+++ 25948083_Wassenaar_L_Project_3/25948083_Wassenaar_L_Project_3/Models/UserModel.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
 
 namespace _25948083_Wassenaar_L_Project_3.Models
 {
     public class UserModel
     {
-        public int user_id { get; set; }
+        public string user_id { get; set; }
         public string username { get; set; }
         public string email { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password Required")]
+        [DisplayName("Password")]
+        [StringLength(55, ErrorMessage = ":Less than 55 characters")]
         public string password { get; set; }
     }
 }
