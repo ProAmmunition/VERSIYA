dd6ab658632dcc0ba8740e791ae9c5d2092354e4
ProAmmunition <37572863+ProAmmunition@users.noreply.github.com>
2018-09-13 15:44:13 +0200

    Uploading of files

--- 25948083_Wassenaar_L_Project_3/25948083_Wassenaar_L_Project_3/Controllers/LogPageController.cs
+++ 25948083_Wassenaar_L_Project_3/25948083_Wassenaar_L_Project_3/Controllers/LogPageController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace _25948083_Wassenaar_L_Project_3.Controllers
 {
     public class LogPageController : Controller
     {
-        // GET: LogPage
+        [HttpGet]
         public ActionResult Commit()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Commit(HttpPostedFileBase file)
+        {
+           if(file.ContentLength > 0)
+            {
+                var file_name = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/Uploads"), file_name);
+                file.SaveAs(path);
+                ViewData["Message"] = "File upload successful";
+            }
+            else
+            {
+                ViewData["Message"] = "File upload error";
+            }
+            return View();
+
         }
     }
 }
