dbaa61e4f47d585b14ebf0fa0bc66b16623e6750
ProAmmunition <37572863+ProAmmunition@users.noreply.github.com>
2018-09-14 15:31:54 +0200

    Downloading of files

--- 25948083_Wassenaar_L_Project_3/25948083_Wassenaar_L_Project_3/Controllers/LogPageController.cs
+++ 25948083_Wassenaar_L_Project_3/25948083_Wassenaar_L_Project_3/Controllers/LogPageController.cs
@@ -9,6 +9,11 @@ namespace _25948083_Wassenaar_L_Project_3.Controllers
 {
     public class LogPageController : Controller
     {
+        public static string file_name;
+        public string file_extension;
+        public double file_size;
+        public double to_mb;
+
         [HttpGet]
         public ActionResult Commit()
         {
@@ -20,10 +25,14 @@ namespace _25948083_Wassenaar_L_Project_3.Controllers
         {
            if(file.ContentLength > 0)
             {
-                var file_name = Path.GetFileName(file.FileName);
+                file_name = Path.GetFileName(file.FileName);
+                file_size = file.ContentLength;
+                to_mb = (file_size / 1024) / 1024;
+                string mb_2_decimal = string.Format("{0:N2}", to_mb);
+                file_extension = Path.GetExtension(file.FileName);
                 var path = Path.Combine(Server.MapPath("~/Uploads"), file_name);
                 file.SaveAs(path);
-                ViewData["Message"] = "File upload successful";
+                ViewData["Message"] = file_name + " [DATE: " + DateTime.Now.ToString("MM / dd / yyyy hh: mm tt") + "] [File size: " + mb_2_decimal + "MB] [File Extension: " + file_extension + "]";
             }
             else
             {
@@ -31,6 +40,25 @@ namespace _25948083_Wassenaar_L_Project_3.Controllers
             }
             return View();
 
+        }
+
+        public ActionResult DownloadFile()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "Uploads/";
+            byte[] bytes = System.IO.File.ReadAllBytes(path + file_name);
+            string fileName = file_name;
+            if (file_extension != null || file_name != null)
+            {
+   
+               ViewData["Message"] = file_name;
+               return File(bytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+                
+            }
+            else
+            {
+                ViewData["Message"] = "No file was selected";
+            }
+            return View("Commit");
         }
     }
 }
