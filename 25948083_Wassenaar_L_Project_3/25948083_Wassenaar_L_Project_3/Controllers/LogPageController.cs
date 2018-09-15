2e3ed5f8059afa093114d44116899e0028a81b67
ProAmmunition <37572863+ProAmmunition@users.noreply.github.com>
2018-09-15 15:53:40 +0200

    Download of existing files

--- 25948083_Wassenaar_L_Project_3/25948083_Wassenaar_L_Project_3/Controllers/LogPageController.cs
+++ 25948083_Wassenaar_L_Project_3/25948083_Wassenaar_L_Project_3/Controllers/LogPageController.cs
@@ -10,10 +10,7 @@ namespace _25948083_Wassenaar_L_Project_3.Controllers
     public class LogPageController : Controller
     {
         public static string file_name;
-        public string file_extension;
-        public double file_size;
-        public double to_mb;
-
+      
         [HttpGet]
         public ActionResult Commit()
         {
@@ -23,42 +20,40 @@ namespace _25948083_Wassenaar_L_Project_3.Controllers
         [HttpPost]
         public ActionResult Commit(HttpPostedFileBase file)
         {
-           if(file.ContentLength > 0)
+           if(file != null)
             {
                 file_name = Path.GetFileName(file.FileName);
-                file_size = file.ContentLength;
-                to_mb = (file_size / 1024) / 1024;
+                double file_size = file.ContentLength;
+                double to_mb = (file_size / 1024) / 1024;
                 string mb_2_decimal = string.Format("{0:N2}", to_mb);
-                file_extension = Path.GetExtension(file.FileName);
+                string file_extension = Path.GetExtension(file.FileName);
                 var path = Path.Combine(Server.MapPath("~/Uploads"), file_name);
                 file.SaveAs(path);
                 ViewData["Message"] = file_name + " [DATE: " + DateTime.Now.ToString("MM / dd / yyyy hh: mm tt") + "] [File size: " + mb_2_decimal + "MB] [File Extension: " + file_extension + "]";
             }
             else
             {
-                ViewData["Message"] = "File upload error";
+                ViewData["Message"] = "No file is chosen";
             }
             return View();
 
         }
 
-        public ActionResult DownloadFile()
+        public ActionResult Downloads()
         {
-            string path = AppDomain.CurrentDomain.BaseDirectory + "Uploads/";
-            byte[] bytes = System.IO.File.ReadAllBytes(path + file_name);
-            string fileName = file_name;
-            if (file_extension != null || file_name != null)
+            var dir = new DirectoryInfo(Server.MapPath("~/Uploads/"));
+            FileInfo [] file_names = dir.GetFiles();
+            List<string> items = new List<string>();
+            foreach (var file in file_names)
             {
-   
-               ViewData["Message"] = file_name;
-               return File(bytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
-                
+                items.Add(file.Name);
             }
-            else
-            {
-                ViewData["Message"] = "No file was selected";
-            }
-            return View("Commit");
+            return View(items);
         }
-    }
+        public FileResult Download(string file_name)
+        {
+            var file_path = "~/Uploads/" + file_name;
+            return File(file_path, "application/force-download", Path.GetFileName(file_path));
+        }
+}
 }
